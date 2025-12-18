7eec8e4ab6e1996e01cc5a1f0c38c44a7bdef0a0
singhroshan07986 <singhroshan07986@gmail.com>
2025-12-18 13:54:29 +0530

    Add files via upload

---
+++ scriptl].js
@@ -0,0 +1,23 @@
+// Qs1 // create a const object called "products" to store a information shows in pictures.
+
+const products = {
+  names: "Canon EOS R50 V ",
+  abouts:
+    " Mirrorless Camera Body withRF-S14-30mm F4-6.3IS STMPZ Lens  (Black)",
+  rating: 4.5,
+  price: 67900,
+  offers: 15,
+};
+console.log(products);
+
+//Qs2// create a const object called "profile" to store information shows in the pictures.
+
+const profile = {
+  username: "@singhroshan7986",
+  experince: "fresher",
+  name: "roshan singh",
+  about: "Focuses on what the user sees and interacts with directly. Developers use HTML for structure, CSS for visual style, and JavaScript for interactivity.",
+
+};
+console.log(profile);
+
