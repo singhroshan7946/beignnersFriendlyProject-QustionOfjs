aed14b47de5073e966272aca59e8763e39eacc6b
singhroshan07986 <singhroshan07986@gmail.com>
2025-12-22 12:27:02 +0530

    Refactor countvowels function implementations
    Refactored countvowels function implementations to remove comments and improve readability.

--- functionBased.js
+++ functionBased.js
@@ -1,39 +1,40 @@
 //Qs1 // create a function using 'function' keyword that takes a string as an argument and returns the number of vowels in that string.
 // method 1
 
-// function countvowels(str) {
-//     const vowels = 'aeiouAEIOU';
-//     let count = 0;
-//     for (let char of str) {
-//         if (vowels.includes(char)) {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+function countvowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
 // method 2
 
-// function countvowels(str) {
-//   let count = 0;
-//   for (let el of str) {
-//     if (
-//       el === "a" ||
-//       el === "e" ||
-//       el === "i" ||
-//       el === "o" ||
-//       el === "u" ||
-//       el === "A" ||
-//       el === "E" ||
-//       el === "I" ||
-//       el === "O" ||
-//       el === "U"
-//     ) {
-//       count++;
-//     }
-//   }
-//   console.log(count);
-// }
+function countvowels(str) {
+  let count = 0;
+  for (let el of str) {
+    if (
+      el === "a" ||
+      el === "e" ||
+      el === "i" ||
+      el === "o" ||
+      el === "u" ||
+      el === "A" ||
+      el === "E" ||
+      el === "I" ||
+      el === "O" ||
+      el === "U"
+    ) {
+      count++;
+    }
+  }
+  console.log(count);
+}
 
+// same function using of arrow function 
 
 let countvowels = (str) => {
 let vowels = 'aeiouAEIOU';
